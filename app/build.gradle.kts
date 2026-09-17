042c8f971e8a262750697fc502439e32da748bac
WaynenyambuyaCEO <waynenyambuya742@gmail.com>
2026-09-17 21:38:10 +0200

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,11 +22,14 @@ android {
     sourceCompatibility = JavaVersion.VERSION_17
     targetCompatibility = JavaVersion.VERSION_17
   }
+  kotlinOptions {
+    jvmTarget = "17"
+  }
   buildFeatures {
     compose = true
   }
   composeOptions {
-    kotlinCompilerExtensionVersion = "1.5.4"
+    kotlinCompilerExtensionVersion = "1.5.8"
   }
 }
 
